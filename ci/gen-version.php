e2c1f92841847912e81979602f7625997849f570
Manish Kumar <manish.br.kumar@oracle.com>
2021-12-14 21:48:18 +0530

    Adding some minor improvement #minor

--- ci/gen-version.php
+++ ci/gen-version.php
@@ -139,6 +139,8 @@ function bumpVersion(SemVer\Version $latestVersion) {
 function tagRepo(SemVer\Version $version) {
 
     $repoUrl = getenv("CI_REPOSITORY_URL");
+    echo "GITHUB_WORKFLOW";
+    echo getenv("GITHUB_WORKFLOW");
     $pushUrl = preg_replace("~.+@([^/]+)\/~", "git@$1:", $repoUrl);
 
     // TODO: use a client to execute the below commands
