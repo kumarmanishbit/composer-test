5dfcc3070aec197beb00b41c28f8fab654d98cc9
Manish Kumar <manish.br.kumar@oracle.com>
2022-02-08 22:17:12 +0530

    #patch adding package.json

--- ci/gen-version.php
+++ ci/gen-version.php
@@ -132,7 +132,7 @@ function bumpVersion(SemVer\Version $latestVersion) {
     }
 
     file_put_contents("environment", $release);
-
+    exec("echo $release > ./environment 2>&1");
     if(!$newVersion->eq(new SemVer\Version($lastRelease))) {
         tagRepo($newVersion);
     }
